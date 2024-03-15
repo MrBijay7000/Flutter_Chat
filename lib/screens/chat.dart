11863ad7a329b0bf180afeb0bf21ead58842019c
BIJAY SHRESTHA <shresthabzay95@gmail.com>
2024-03-15 15:32:09 +0545

    Firebase Added

---
+++ lib/screens/chat.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/widgets/chat_message.dart';
+import 'package:flutter_chat/widgets/new_message.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('FlutterChat'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            )
+          ],
+        ),
+        body: const Column(
+          children: [
+            Expanded(child: ChatMessages()),
+            NewMessage(),
+          ],
+        ));
+  }
+}
